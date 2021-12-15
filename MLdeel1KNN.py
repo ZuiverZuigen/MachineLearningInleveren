8f0c75ff7cb358645a668da5b23fb882d85bf1da
ZuiverZuigen <squidslammer@gmail.com>
2021-12-15 10:07:40 +0100

    allah

--- MLdeel1KNN.py
+++ MLdeel1KNN.py
@@ -1,4 +1,4 @@
-import pandas as pd #test test test
+import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
