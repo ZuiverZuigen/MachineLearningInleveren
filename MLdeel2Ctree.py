a056535190daf1023dcfb46fd09b6af35dc66f31
ZuiverZuigen <squidslammer@gmail.com>
2022-01-07 15:53:23 +0100

    Rastaclaat

--- MLdeel2Ctree.py
+++ MLdeel2Ctree.py
@@ -9,8 +9,49 @@ from dmba import plotDecisionTree, classificationSummary, regressionSummary
 
 wine_df = dmba.load_data('Wine.csv')
 
-classTree = DecisionTreeClassifier(random_state=0)
-classTree.fit(wine_df.drop(columns=['Type']), wine_df['Type'])
+X = wine_df.drop(columns=['Type'])
+y = wine_df['Type']
+train_X, valid_X, train_y, valid_y = train_test_split(X, y, test_size=0.4, random_state=1)
 
-print("Classes: {}".format(', '.join(classTree.classes_)))
-plotDecisionTree(classTree, feature_names=wine_df.columns[:2], class_names=classTree.classes_)
+fullClassTree = DecisionTreeClassifier()
+fullClassTree.fit(train_X, train_y)
+
+plotDecisionTree(fullClassTree, feature_names=train_X.columns)
+
+classificationSummary(train_y, fullClassTree.predict(train_X))
+classificationSummary(valid_y, fullClassTree.predict(valid_X))
+
+# Five-fold cross-validation of the full decision tree classifier
+treeClassifier = DecisionTreeClassifier()
+
+scores = cross_val_score(treeClassifier, train_X, train_y, cv=5)
+print('Accuracy scores of each fold: ', [f'{acc:.3f}' for acc in scores])
+print(f'Accuracy: {scores.mean():.3f} (+/- {scores.std() * 2:.3f})')
+print(f'Accuracy: {scores.mean():.3f} (+/- {scores.std():.3f})')
+
+# Start with an initial guess for parameters
+param_grid = {
+    'max_depth': [10, 20, 30, 40],
+    'min_samples_split': [20, 40, 60, 80, 100],
+    'min_impurity_decrease': [0, 0.0005, 0.001, 0.005, 0.01],
+}
+gridSearch = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=5, n_jobs=-1)
+gridSearch.fit(train_X, train_y)
+print('Initial score: ', gridSearch.best_score_)
+print('Initial parameters: ', gridSearch.best_params_)
+
+# Adapt grid based on result from initial grid search
+param_grid = {
+    'max_depth': list(range(2, 16)),
+    'min_samples_split': list(range(10, 22)),
+    'min_impurity_decrease': [0.0009, 0.001, 0.0011],
+}
+gridSearch = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=5, n_jobs=-1)
+gridSearch.fit(train_X, train_y)
+print('Improved score: ', gridSearch.best_score_)
+print('Improved parameters: ', gridSearch.best_params_)
+
+bestClassTree = gridSearch.best_estimator_
+
+plotDecisionTree(bestClassTree, feature_names=train_X.columns)
+
