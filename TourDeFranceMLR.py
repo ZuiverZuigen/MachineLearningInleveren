077fbddbf6e97ee5e30634f0c82f4ebed92cf733
ZuiverZuigen <squidslammer@gmail.com>
2022-01-23 19:18:22 +0100

    rastaclaat

--- TourDeFranceMLR.py
+++ TourDeFranceMLR.py
@@ -12,10 +12,16 @@ from surprise.model_selection import train_test_split
 import dmba
 
 #load and preproces the data set
-big_df = pd.read_csv (r'C:\Users\marwi\Desktop\data\results\Results2021CatWT.csv')
+big2021_df = pd.read_csv (r'C:\Users\marwi\Desktop\data\results\Results2021CatWT.csv')
 pd.set_option('max_columns', None)
 #print(big_df)
 
-#UAE Tour stage 1
-df_UAE = big_df.iloc[:139]
-print(df_UAE)
+#slice df tour de france
+Tour2021_df = big2021_df.iloc[18397:22377]
+print(Tour2021_df)
+
+#Treat Stage_Name as categorical, convert to dummy variables
+big_df['Stage_Name'] = big_df['Stage_Name'].astype('category')
+new_categories = {1: 'Undergrad', 2: 'Graduate', 3: 'Advanced/Professional'}
+bank_df.Education.cat.rename_categories(new_categories, inplace=True)
+bank_df = pd.get_dummies(bank_df, prefix_sep='_', drop_first=True)
