bb15d90b592356e03ff917d8c0b03eb552f6c871
marwi <marwinaerden@hotmail.com>
2022-01-24 18:13:06 +0100

    Rastaclaat

--- TourDeFranceMLR.py
+++ TourDeFranceMLR.py
@@ -2,6 +2,7 @@ import heapq
 from collections import defaultdict
 
 import pandas as pd
+from sklearn import preprocessing
 import matplotlib.pylab as plt
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
@@ -13,15 +14,70 @@ import dmba
 
 #load and preproces the data set
 big2021_df = pd.read_csv (r'C:\Users\marwi\Desktop\data\results\Results2021CatWT.csv')
+riderInfos_df = pd.read_csv(r'C:\Users\marwi\Desktop\data\rider_infos.csv')
 pd.set_option('max_columns', None)
 #print(big_df)
 
 #slice df tour de france
-Tour2021_df = big2021_df.iloc[18397:22377]
-print(Tour2021_df)
+Tour2021_df = big2021_df.loc[18397:22377]
+print(riderInfos_df)
 
-#Treat Stage_Name as categorical, convert to dummy variables
-big_df['Stage_Name'] = big_df['Stage_Name'].astype('category')
-new_categories = {1: 'Undergrad', 2: 'Graduate', 3: 'Advanced/Professional'}
-bank_df.Education.cat.rename_categories(new_categories, inplace=True)
-bank_df = pd.get_dummies(bank_df, prefix_sep='_', drop_first=True)
+
+def get_top_n(predictions, n=20):
+    # First map the predictions to each user.
+    byUser = defaultdict(list)
+    for p in predictions:
+        byUser[p.uid].append(p)
+
+    # For each user, reduce predictions to top-n
+    for uid, userPredictions in byUser.items():
+        byUser[uid] = heapq.nlargest(n, userPredictions, key=lambda p: p.est)
+    return byUser
+
+# Convert thes data set into the format required by the surprise package
+reader = Reader(Rnk_scale=(181, 1))
+data = Dataset.load_from_df(Tour2021_df[['Stage#', 'RennerID', 'Rnk']], reader)
+
+# Split into training and test set
+trainset, testset = train_test_split(data, test_size=.25, random_state=1)
+
+## User-based filtering
+# compute cosine similarity between users
+sim_options = {'name': 'cosine', 'user_based': True}
+algo = KNNBasic(sim_options=sim_options)
+algo.fit(trainset)
+
+# Than predict ratings for all pairs (u, i) that are NOT in the training set.
+predictions = algo.test(testset)
+
+top_n = get_top_n(predictions, n=15)
+
+# Print the recommended items for each user
+print()
+print('Top-4 recommended items for each user')
+for uid, user_ratings in list(top_n.items())[:16]:
+    print('User {}'.format(uid))
+    for prediction in user_ratings:
+        print('  Item {0.iid} ({0.est:.2f})'.format(prediction), end='')
+    print()
+print()
+
+## Item-based filtering
+# compute cosine similarity between users
+sim_options = {'name': 'cosine', 'user_based': False}
+algo = KNNBasic(sim_options=sim_options)
+algo.fit(trainset)
+
+# Than predict ratings for all pairs (u, i) that are NOT in the training set.
+predictions = algo.test(testset)
+top_n = get_top_n(predictions, n=15)
+
+# Print the recommended items for each user
+print()
+print('Top-4 recommended items for each user')
+for uid, user_ratings in list(top_n.items())[:16]:
+    print('User {}'.format(uid))
+    for prediction in user_ratings:
+        print('  Item {0.iid} ({0.est:.2f})'.format(prediction), end='')
+    print()
+
